5ff30863a7d389d0450f663daf6f873203fe5480
Hritik-Kumar-dev <hritikkumar.dev77@gmail.com>
2026-04-07 22:57:05 +0530

    function easy

--- functions/prime.cpp
+++ functions/prime.cpp
@@ -3,20 +3,24 @@ using namespace std;
 
 
 void factor(int n){
+    int count=0 ; 
 for (int i = n; i >= 1; i--)
     {
         if (n%i==0)
         {
-            cout<< i <<" " ;
+            count++;
         }
         
+    }
+    if(count==2){
+        cout<< " Prime" <<endl;
+    }else{
+        cout<< "not prime " <<endl;
     }
     cout<< "\n";
     
 }
 int main() {
-
-
 
     int n ; 
     cin>> n ;

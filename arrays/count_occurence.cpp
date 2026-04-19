a8a2859a7174c8c18fcfd88a0d42e14ac527eb84
Hritik-Kumar-dev <hritikkumar.dev77@gmail.com>
2026-04-19 12:22:51 +0530

    count occurence

---
+++ arrays/count_occurence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+     int n  ; 
+    cin>> n ;
+    int target ; 
+    cin>> target  ;
+     long long arr[n] ;
+     int count = 0 ;
+     
+     for (int i = 0; i < n; i++)
+     {
+        cin>> arr[i] ;
+     }
+     
+     for (int  i = 0; i < n; i++)
+     {
+        if (arr[i]==target)
+        {
+            count++ ; 
+        }
+     }
+     
+     cout<< count <<endl;
+
+    return 0;
+}

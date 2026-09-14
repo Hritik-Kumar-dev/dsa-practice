8b78415daef5a4999ec88264b948f0a21a2c7f26
Hritik-Kumar-dev <hritikkumar.dev77@gmail.com>
2026-09-14 09:00:06 +0530

    sort o and 1

---
+++ arrays/sort_01.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int noofarray;
+    cin >> noofarray;
+    for (int i = 0; i < noofarray; i++)
+    {
+        /* code */
+    
+    
+
+
+    int n ; 
+    cin >> n;
+    int arr[n];
+    int zeros=0;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]< 0|| arr[i]>1)
+        {
+            cout<<"only binary are allowed" <<endl ;
+            break;
+        }else if (arr[i]==0){
+            zeros++ ;
+        }
+    }
+     for (int i = 0; i < n; i++)
+     {
+         if(i<zeros){
+            cout << 0<<" ";
+         }else{
+
+             cout << 1 <<" ";
+            }
+         
+     }
+     
+    }
+    
+    return 0;
+}
